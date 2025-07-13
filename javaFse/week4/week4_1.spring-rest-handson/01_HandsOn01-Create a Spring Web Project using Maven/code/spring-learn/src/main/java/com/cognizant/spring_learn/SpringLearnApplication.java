d2398705c86e2f7540077a6bc2d5fadb49061aac
Kubera_Eswaran_S <117847641+KuberaEswaran@users.noreply.github.com>
2025-07-13 22:07:23 +0530

    Week4 file added

---
+++ javaFse/week4/week4_1.spring-rest-handson/01_HandsOn01-Create a Spring Web Project using Maven/code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] a) {
+        SpringApplication.run(SpringLearnApplication.class, a);
+        System.out.println("Application Starts");
+    }
+
+}
